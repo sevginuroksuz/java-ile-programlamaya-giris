f1e34fdae08e7eec6ccf445bcc0e9f01c06ee5f6
Sevgi Nur Öksüz <90787721+sevginuroksuz@users.noreply.github.com>
2023-07-24 04:49:28 +0300

    Add files via upload

---
+++ Java ile Programlamaya Giriş/javademos/conditionals/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Diffensive Programming: Savunmacı Programlama. Olabilecek tüm şartlara karşılık gösteren program geliştirme yöntemi.
+        int sayi = 24;
+        if (sayi< 20){
+            System.out.println("Sayi 20 den küçüktür.");
+        } else if (sayi==20) {
+            System.out.println("Sayi 20'ye eşittir.");
+
+        } else{
+            System.out.println("Sayi 20 den küçük değildir.");
+        }
+    }
+}
