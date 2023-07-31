649ae8b57677b7213b6d3149cc7e97c67843cbbb
sevginuroksuz <sevginuroksuz@gmail.com>
2023-07-31 12:52:55 +0300

    commit

---
+++ javademos/recapDemoClasses/src/Dortİslem.java
@@ -0,0 +1,17 @@
+public class Dortİslem {
+    public int Topla(int sayi1, int sayi2){
+        return sayi1 + sayi2;
+    }
+
+    public int Cikar(int sayi1, int sayi2){
+        return sayi1 - sayi2;
+    }
+
+    public int Carp(int sayi1, int sayi2){
+        return sayi1 * sayi2;
+    }
+
+    public int Bol(int sayi1, int sayi2){
+        return sayi1 / sayi2;
+    }
+}
---
+++ javademos/recapDemoClasses/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Dortİslem dortIslem = new Dortİslem();
+        int sonuc = dortIslem.Topla(3,4);
+        System.out.println(sonuc);
+    }
+}
